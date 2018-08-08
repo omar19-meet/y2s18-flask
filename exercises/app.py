8f34bcc742c7f9436cb658d97a50c16c5e0ddb95
meet <omar19@meet.mit.edu>
2018-08-08 05:10:26 -0400

    lol

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
-
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    fav_players=["Salah","Mbappe","Ronaldo"]
+    likes_same_sport=True
+    return render_template("index.html",fav_players=fav_players,
+     likes_same_sport=likes_same_sport)
 
 if __name__ == '__main__':
    app.run(debug = True)
